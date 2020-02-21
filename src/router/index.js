30c53d443722c10526fb5a7782a0ae1141dff38b
= <awhank@gmail.com>
2020-02-21 17:16:15 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+
 Vue.use(VueRouter);
 
 const routes = [
+
   {
     path: "/",
     name: "Home",
